22cebdff6651977dea92422f0dd7cb2af214ec1e
Tanvi <rmtanvikrishnan@gmail.com>
2025-08-31 00:45:11 +0530

    backend updates

--- backend/src/users/user.model.js
+++ backend/src/users/user.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
@@ -16,6 +17,21 @@ const userSchema = new Schema({
     default: Date.now
   }
 });
+
+
+//hasing passwords
+userSchema.pre('save' ,async function(next){
+  const user = this;
+  if(!user.isModified('password')) return next();
+  const hashedPassword = await bcrypt.hash(user.password,10);
+  user.password=hashedPassword;
+  next();
+})
+// match password
+userSchema.methods.comparePassword = function (candidatePassword){
+  return bcrypt.compare(candidatePassword,this.password);
+}
+
 
 // ✅ Make sure spelling matches here
 const User = model('User', userSchema);
--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,18 +1,44 @@
 const express = require('express');
-const User = require('./user.model');  
+const User = require('./user.model');
+const genrateToken = require('../middleware/generateToken');
 const router = express.Router();
+
 
 //register endpoint
 
-router.post('/register',async(req,res)=>{
-    try{
-        const {username,email,password} = req.body;
-        const user = new User({email,username,password});
+router.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ email, username, password });
         await user.save();
-        res.status(201).send({message:"user registered successfully!"})
-    }catch(error){
-         console.error("❌ Error in /register:", error);
-  res.status(500).send({ error: "Internal server error" });
+        res.status(201).send({ message: "user registered successfully!" })
+    } catch (error) {
+        console.error("❌ Error in /register:", error);
+        res.status(500).send({ error: "Internal server error" });
+    }
+})
+//login user endpoint
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' })
+        }
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).send({ message: 'Password not match' })
+        }
+        const token = await genrateToken(user.id)
+        console.log("token",token)
+
+        res.cookie('token',token)
+
+
+        res.status(200).send({ message: "Logged in successfully", user })
+    } catch (error) {
+        console.error("❌ Error logged in user", error);
+        res.status(500).send({ error: "error logged in user" });
     }
 })
 
