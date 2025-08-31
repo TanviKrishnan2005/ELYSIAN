ba2f5a8f3c128185fe588ea42eed8fd77702a279
Tanvi <rmtanvikrishnan@gmail.com>
2025-08-31 22:19:40 +0530

    logout endpoint/ delete user added

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const User = require('./user.model');
 const genrateToken = require('../middleware/generateToken');
+const verifyToken = require('../middleware/verifyToken');
 const router = express.Router();
+
 
 
 //register endpoint
@@ -30,16 +32,51 @@ router.post('/login', async (req, res) => {
             return res.status(401).send({ message: 'Password not match' })
         }
         const token = await genrateToken(user.id)
-        console.log("token",token)
+        
 
-        res.cookie('token',token)
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:true,
+            sameSite:'None'
+        })
 
 
-        res.status(200).send({ message: "Logged in successfully", user })
+        res.status(200).send({ message: "Logged in successfully",token, user:{
+            id :user.id,
+            email:user.email,
+            username:user.username,
+            role:user.role,
+            profileImage: user.userImage,
+            bio:user.bio,
+            profession:user.profession
+        } })
     } catch (error) {
         console.error("❌ Error logged in user", error);
         res.status(500).send({ error: "error logged in user" });
     }
 })
+
+//logout endpoints
+router.post('/logout',(req,res)=>{
+    res.clearCookie('token');
+    res.status(200).send({message: "logout successful"})
+})
+
+//delete a user 
+
+router.delete('/users/:id',async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const user = await User.findByIdAndDelete(id);
+        if(!user){
+            return res.status(404).send({message:'User not found'})
+        }
+        res.status(200).send({message:"user deleted successfully"})
+    } catch (error) {
+        console.error("❌ Error deleting user", error);
+        res.status(500).send({ error: "error deleting user" });
+    }
+})
+
 
 module.exports = router;
