282324f3e9bbc785af74b1ed4537d8d05d85e4b3
Tanvi <rmtanvikrishnan@gmail.com>
2026-01-03 20:08:39 +0530

    stripe frontend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./tailwind.css";
-import "./App.css";
-
 import { RouterProvider } from "react-router-dom";
-import router from "./routers/router.jsx";
-
-import "remixicon/fonts/remixicon.css";
-
 import { Provider } from "react-redux";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+import router from "./routers/router.jsx";
 import { store } from "./redux/store.js";
 
 import { Toaster } from "react-hot-toast";
 
+import "./tailwind.css";
+import "./App.css";
+import "remixicon/fonts/remixicon.css";
+
+// 🔑 Stripe public key
+const stripePromise = loadStripe(
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
-      <Toaster position="top-right" reverseOrder={false} />
+      <Elements stripe={stripePromise}>
+        <RouterProvider router={router} />
+        <Toaster position="top-right" reverseOrder={false} />
+      </Elements>
     </Provider>
   </StrictMode>
 );
