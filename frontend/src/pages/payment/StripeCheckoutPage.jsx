ae3300a957fe85d09f76e972ab114024b06facd7
Tanvi <rmtanvikrishnan@gmail.com>
2026-06-08 18:44:48 +0530

    fix stripe elements remount

--- frontend/src/pages/payment/StripeCheckoutPage.jsx
+++ frontend/src/pages/payment/StripeCheckoutPage.jsx
@@ -4,6 +4,11 @@ import { Elements } from "@stripe/react-stripe-js";
 import Checkout from "./Checkout";
 
 const stripePromise = loadStripe(
+  
+  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+);
+console.log(
+  "STRIPE KEY:",
   import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
 );
 
@@ -14,14 +19,11 @@ const StripeCheckoutPage = () => {
   const { clientSecret, items, totalAmount } = location.state || {};
   console.log("CLIENT SECRET:", clientSecret);
 
+ 
+
   if (!clientSecret) {
   return <p>Loading payment...</p>;
 }
-
-  if (!clientSecret || !items || !totalAmount) {
-    navigate("/dashboard/user/orders");
-    return null;
-  }
 
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-white rounded shadow">
@@ -29,17 +31,20 @@ const StripeCheckoutPage = () => {
         Secure Payment
       </h2>
 
-      <Elements stripe={stripePromise} options={{ clientSecret }}>
-        <Checkout
-          orderData={{ items, totalAmount }}
-          onSuccess={() => navigate("/dashboard/user/orders")}
-        />
-      </Elements>
+      {clientSecret && (
+  <Elements
+    stripe={stripePromise}
+    options={{ clientSecret }}
+    key={clientSecret}
+  >
+    <Checkout
+      orderData={{ items, totalAmount }}
+      onSuccess={() => navigate("/dashboard/user/orders")}
+    />
+  </Elements>
+)}
     </div>
   );
 };
-console.log(
-  "STRIPE KEY:",
-  import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
-);
+
 export default StripeCheckoutPage;
