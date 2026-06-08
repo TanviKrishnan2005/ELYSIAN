14fe64140119ee2b3e921e65f41c9b1a4bf3c77f
Tanvi <rmtanvikrishnan@gmail.com>
2026-06-08 17:17:41 +0530

    production backend config

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,13 @@ app.use("/api/stripe", stripeWebhook);
 app.use(express.json({ limit: "25mb" }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use
+cors({
+  origin: [
+    "http://localhost:5173",
+  ],
+  credentials: true,
+})
 
 /* =========================
    ROUTES
